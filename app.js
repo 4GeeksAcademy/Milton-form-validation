7428e8caede83ef92167b1160f890cee54690544
Amat98 <milton.yv98@gmail.com>
2024-08-07 16:04:58 -0500

    Aumentando validacion para el formulario, el alert se quita o se pone dependiendo si todos los campos estan completos

---
+++ app.js
@@ -0,0 +1,32 @@
+let inputs = document.querySelectorAll(".form-control");
+let btnSubmit = document.querySelector("#btn-submit");
+let alert = document.querySelector(".alert");
+let btnCancel = document.querySelector("#btn-cancel");
+
+let controlFrom = (e) => {
+  e.preventDefault();
+};
+btnSubmit.addEventListener("click", (e) => {
+  e.preventDefault();
+  let valaditons = 0;
+  for (let i = 0; i < inputs.length; i++) {
+    if (inputs[i].tagName === "SELECT" && inputs[i].value === "Pick a state") {
+      inputs[i].style.backgroundColor = "rgb( 255, 173, 173)";
+    } else {
+      if (inputs[i].value === "") {
+        alert.style.display = "block";
+        inputs[i].style.backgroundColor = " rgb( 255, 173, 173  )";
+      } else {
+        valaditons++;
+        inputs[i].style.backgroundColor = "white";
+      }
+    }
+  }
+  if (valaditons === 9) {
+    alert.style.display = "none";
+  }
+});
+
+btnCancel.addEventListener("click", () => {
+  location.reload();
+});
